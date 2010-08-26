af0be3775c05e2907c05ec87849ef2cc67038282
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2010-08-26 14:09:15 -0400

    sched rename struct task in_iowait field to sched_in_iowait
    Rename task struct in_iowait to sched_in_iowait, since it's only
    scheduler-specific. Extracted from a patch originally from Peter Zijlstra.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5078,9 +5078,9 @@ void __sched io_schedule(void)
 
 	delayacct_blkio_start();
 	atomic_inc(&rq->nr_iowait);
-	current->in_iowait = 1;
+	current->sched_in_iowait = 1;
 	schedule();
-	current->in_iowait = 0;
+	current->sched_in_iowait = 0;
 	atomic_dec(&rq->nr_iowait);
 	delayacct_blkio_end();
 }
@@ -5093,9 +5093,9 @@ long __sched io_schedule_timeout(long timeout)
 
 	delayacct_blkio_start();
 	atomic_inc(&rq->nr_iowait);
-	current->in_iowait = 1;
+	current->sched_in_iowait = 1;
 	ret = schedule_timeout(timeout);
-	current->in_iowait = 0;
+	current->sched_in_iowait = 0;
 	atomic_dec(&rq->nr_iowait);
 	delayacct_blkio_end();
 	return ret;
